6ed21a5e4bb3bea6d0c975965d81d3b8a01c3b6d
Rogério Nascimento <87660080+Rogerio-Nascimento@users.noreply.github.com>
2021-08-17 11:47:36 -0300

    alterações

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function verificar (){
     var n2 = window.document.getElementById("nt2")
     var tr = window.document.getElementById("ntt")
     var res = window.document.getElementById ("res")
-
     var tot = Number(n1.value) * 0.4 + Number(n2.value)*0.6 *0.8 + Number(tr.value)*0.2
-    res.innerHTML  = `sua nota foi: ${tot}`
+    
+    if (n1.value == 0 || n2.value ==0 || tr.value ==0){
+        alert("Insira os dados corretamente")
+    }
+    else{
+    
+    res.innerHTML  = `sua nota foi: ${tot}`}
 }
