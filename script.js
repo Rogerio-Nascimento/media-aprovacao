44eb939caec5403c1a07da99b63282d7fef6535c
Rogério Nascimento <87660080+Rogerio-Nascimento@users.noreply.github.com>
2021-08-18 10:10:29 -0300

    commit

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ function verificar (){
     var res = window.document.getElementById ("res")
     var tot = Number(n1.value) * 0.4 + Number(n2.value)*0.6 *0.8 + Number(tr.value)*0.2
     
-    if (n1.value == 0 || n2.value ==0 || tr.value ==0){
+    if (n1.value == 0 || n2.value == 0 || tr.value == 0){
         alert("Insira os dados corretamente")
+    }
+
+    else if (tot < 6){
+        res.innerHTML = `Sua nota foi: ${tot} <font color="red"><strong>você está de recuperação</strong>`
+    }
+
+
+    else if (tot >= 6){
+        res.innerHTML = `Parabens, sua nota foi ${tot} <font color="green"> <strong>você foi aprovado</strong> `
     }
     else{
     
-    res.innerHTML  = `sua nota foi: ${tot}`}
+    res.innerHTML  = ` Verifique os dados corretamente`}
+
 }
+    
